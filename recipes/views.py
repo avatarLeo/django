967221b5f075d9dc40b0fb3a9222a48211e4bb1b
avatarLeo <leandroalmeida055@gmail.com>
2026-05-12 06:45:52 -0300

    aula 60

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Category, Recipe
 
+#{% url 'recipes:category' recipe.category.id %}
+
+def category(request, category_id):
+    
+    recipe = Recipe.objects.filter(category__id=category_id, is_published=True)
+
+    return render(request, 'recipes/pages/category.html', context={
+        "recipe":recipe
+    })
 
 def home(request):
+    
+    recipe = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        "recipe":recipe
     })
 
 
 def recipe(request, id):
+
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
-        'is_detail_page': True,
+        "recipe":recipe
     })
