f90b1fcb04e77292e4b547acf8426942c972e602
avatarLeo <leandroalmeida055@gmail.com>
2026-05-22 20:16:28 -0300

    modifiquei o recipes/admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import Category, Recipe
 
-class CategoryAdmin(admin.ModelAdmin):
-    ...
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     ...
 
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+    
 admin.site.register(Category, CategoryAdmin)
