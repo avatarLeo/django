e789bb77a227421101542ffaba6251a96764f1d0
avatarLeo <leandroalmeida055@gmail.com>
2026-07-09 11:57:04 -0300

    Aula 85

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
+from recipes.models import Category, Recipe
+from django.contrib.auth.models import User
 
-class RecipeViewsTest(TestCase):
+from .test_recipe_base import RecipeTestBase
+
+
+class RecipeViewsTest(RecipeTestBase):
 
     def test_view_home_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
@@ -23,6 +28,10 @@ class RecipeViewsTest(TestCase):
             response.content.decode('utf-8')
         )
     def test_recipe_home_templates_loads_recipes(self):
+        self.make_recipe()        
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        self.assertIn('Recipe title', content)
         ...
 
     #category
@@ -31,12 +40,21 @@ class RecipeViewsTest(TestCase):
         self.assertIs(view.func, views.category)
 
     def test_recipe_category_view_return_status_code_404_ok(self):
-        response = self.client.get(reverse('recipes:category', kwargs={'category_id':1}))
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id':1000}))
         self.assertEquals(response.status_code, 404)
 
-    # def test_recipe_category_view_loads_correct_template(self):
-    #     response = self.client.get(reverse('recipes:category', kwargs={'category_id':1}))
-    #     self.assertTemplateUsed(response, 'recipes/pages/category.html')
+    def test_recipe_category_view_loads_correct_template(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id':1}))
+        self.assertTemplateUsed(response, 'recipes/pages/category.html')
+
+    def test_recipe_category_templates_loads_recipe(self):
+            needed_category_test = 'This is a test category'
+            self.make_recipe(title=needed_category_test)
+            response = self.client.get(reverse('recipes:category', kwargs={'category_id':1}))
+            content = response.content.decode('utf-8')
+
+            self.assertIn(needed_category_test, content)
 
     def test_recipe_category_template_show_no_recipes_found_if_no_recipe(self):
         response = self.client.get(reverse('recipes:category', kwargs={'category_id':1000}))
@@ -47,16 +65,17 @@ class RecipeViewsTest(TestCase):
 
     #detail_recipe
     def test_view_recipe_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1000}))
         self.assertIs(view.func, views.recipe)
 
     def test_recipe_view_return_status_code_404_ok(self):
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id':1}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id':1000}))
         self.assertEquals(response.status_code, 404)
 
-    # def test_recipe_view_loads_correct_template(self):
-    #     response = self.client.get(reverse('recipes:category', kwargs={'id':1}))
-    #     self.assertTemplateUsed(response, 'recipes/pages/recipe-view.html')
+    def test_recipe_view_loads_correct_template(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:category', kwargs={'id':1}))
+        self.assertTemplateUsed(response, 'recipes/pages/recipe-view.html')
 
     def test_recipe__template_show_no_recipes_found_if_no_recipe(self):
         response = self.client.get(reverse('recipes:recipe', kwargs={'id':1000}))
@@ -64,3 +83,14 @@ class RecipeViewsTest(TestCase):
             'Not Found',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_detail_templates_loads_correct_recipe(self):
+        needed_category_test = 'This is a detail page'
+        self.make_recipe(title=needed_category_test)
+        response = self.client.get(
+                reverse('recipes:recipe', kwargs={'id':1})
+            )
+        content = response.content.decode('utf-8')
+
+        self.assertIn(needed_category_test, content)
+    
